b55ba792e3bab3707227a65b080947c92b1db367
AnaBondiguel <97415682+AnaBondiguel@users.noreply.github.com>
2022-04-07 18:20:35 +1000

    Add files via upload

---
+++ Raining.rb
@@ -0,0 +1,50 @@
+# puts "Is it raining today?"
+# raining = gets.chomp
+# if raining == "yes"
+#   puts "raining"
+# else 
+#   puts "not raining"
+# end
+# puts "What's the temperature today?"
+# temperature = gets.chomp
+# temperature = temperature.to_i
+# if temperature < 15 && raining == "yes"
+#        puts "It's wet and cold"
+# elsif temperature < 15 && raining == "no"
+#        puts "It's not raining but cold"
+# elsif temperature >= 15 && raining == "no"
+#        puts "It's warm but not rainning"
+# else
+#   puts "It's warm and raining"
+# end
+
+def raining_weather
+  puts "Is it raining today?"
+  raining = gets.chomp
+if raining == "yes"
+  puts "raining"
+else 
+  puts "not raining"
+  end
+  return raining
+end
+
+def temperature_weather
+  puts "What's the temperature today?"
+  temperature = gets.chomp
+  temperature = temperature.to_i
+
+if temperature < 15 && raining_weather == "yes"
+       puts "It's wet and cold"
+elsif temperature < 15 && raining_weather == "no"
+       puts "It's not raining but cold"
+elsif temperature >= 15 && raining_weather == "no"
+       puts "It's warm but not rainning"
+else
+    puts "It's warm and raining"
+  end
+  return temperature
+end  
+
+raining_weather
+temperature_weather
